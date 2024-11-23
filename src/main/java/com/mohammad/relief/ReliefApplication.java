75920609775aa2144d4b1435f3eb61c77d1f0522
mohammadalibashiri5 <mohammadalibashiri5@yahoo.com>
2024-11-23 10:54:07 +0100

    first initialization

---
+++ src/main/java/com/mohammad/relief/ReliefApplication.java
@@ -0,0 +1,13 @@
+package com.mohammad.relief;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReliefApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReliefApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mohammad/relief/ReliefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohammad.relief;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReliefApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
